288921657dacd23d933991aa1292cf1b2ba49a50
hana510 <101069143+hana510@users.noreply.github.com>
2022-03-06 13:50:19 -0800

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+$("body").hide();
+
+let password = prompt("Enter password");
+
+if (password === 'hanabana2010') {
+    $("body").show();
+}
+
 $(".purple-text").hover(
     function(){
 		$( this ).css("color","#77ff33");
